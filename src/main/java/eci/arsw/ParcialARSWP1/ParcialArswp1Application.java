948632cec8195a313d91df6f4f9f27ec4e8ef7bb
unknown <jaider.vargas-n@labinfo.is.escuelaing.edu.co>
2025-03-01 10:09:59 -0500

    initialzr

---
+++ src/main/java/eci/arsw/ParcialARSWP1/ParcialArswp1Application.java
@@ -0,0 +1,13 @@
+package eci.arsw.ParcialARSWP1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcialArswp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParcialArswp1Application.class, args);
+	}
+
+}
---
+++ src/test/java/eci/arsw/ParcialARSWP1/ParcialArswp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package eci.arsw.ParcialARSWP1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcialArswp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
